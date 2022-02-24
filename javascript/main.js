ae0c9c2c0f734284e2bcc4b43b2d0dfce52a305d
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-02-24 11:22:28 +0400

    bug fix

--- javascript/main.js
+++ javascript/main.js
@@ -1,44 +1,40 @@
-let allTasks = [];
+ let allTasks = [];
 
 
-/**
- * This function references to the form elements and assings the values to an Json object stores the Json object in an array. The array is stored in the browser.
- * 
- */
-function createTask() {
-    let description = document.getElementById('description').value;
-    let title = document.getElementById('title').value;
-    let date = document.getElementById('date').value;
-    let catergory = document.getElementById('catergory').value;
-    let text = document.getElementById('text').value;
-    let urgency = document.getElementById('urgency').value;
+ /**
+  * This function references to the form elements and assings the values to an Json object stores the Json object in an array. The array is stored in the browser.
+  * 
+  */
+ function createTask() {
+     let title = document.getElementById('title').value;
+     let date = document.getElementById('date').value;
+     let catergory = document.getElementById('catergory').value;
+     let text = document.getElementById('text').value;
+     let urgency = document.getElementById('urgency').value;
+     let task = {
+         'title': title,
+         'date': date,
+         'catergory': catergory,
+         'text': text,
+         'urgency': urgency,
+         'createdAt': new Date().getTime()
+     }
+
+     allTasks.push(task);
+     allTaskAsString = JSON.stringify(allTasks);
+     localStorage.setItem('allTasks', allTaskAsString);
+
+ }
+
+ /**
+  * This function accesses the stored data from the Browser and loads it again in the array
+  * 
+  */
+ function LoadAllTasks() {
+     let allTaskAsString = localStorage.getItem('allTasks');
+     allTasks = JSON.parse(allTaskAsString);
+     console.log(allTasks);
 
 
-    let task = {
-        'description': description,
-        'title': title,
-        'date': date,
-        'catergory': catergory,
-        'text': text,
-        'urgency': urgency,
-        'createdAt': new Date().getTime()
-    }
-
-
-    console.log(title);
-}
-
-// allTasks.push(task);
-
-// let allTaskAsString = JSON.stringify(allTasks);
-// localStorage.setItem('allTasks', allTaskAsString);
-
-/**
- * This function accesses the stored data from the Browser and loads it again in the array
- * 
- */
-// function LoadAllTasks() {
-//     let allTaskAsString = localStorage.getItem('allTasks');
-//     allTasks = JSON.parse(allTaskAsString);
-//     includeHTML()
-// }
+     includeHTML()
+ }
