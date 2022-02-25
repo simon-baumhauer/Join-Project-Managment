fbf5a134b18467151ceb5ccaffa1995467caa864
JuergenHildbrand <jhildbrand@pm.me>
2022-02-25 15:12:33 +0100

    new

--- javascript/board.js
+++ javascript/board.js
@@ -15,12 +15,38 @@ function renderTasks() {
     for (let i = 0; i < toDo.length; i++) {
         let task = toDo[i];
         renderTask.innerHTML += `
-            <div class="tasks">
+            <div class="tasks" onclick="openTask(${i})">
                 ${task['title']}
                 <img class="delete" onclick="deleteTask(${task})" src="img/x.ico"> 
             </div>    
         `;
     }
+}
+
+function openTask(i) {
+    currentTask = i;
+    document.getElementById('overlayBg').classList.remove('d-none');
+    let task = toDo[currentTask];
+    document.getElementById('openTask').innerHTML = `
+        <div class="openTask" id="openTask">
+            <div class="date">
+                <div>Due Date${task['date']}</div>
+                <div>Created On ${task['createdAt']}</div>
+            </div> 
+            <div>Urgency:  <span class="red">${task['urgency']}</span></div>   
+            <div class="title">${task['title']}</div>
+            <div>${task['text']}</div>
+            <div class="footerTask">
+                <div>Category: <span class="catogery">${task['catergory']}</span></div>
+                <img class="assignedEmployees" src=${task['assignedEmployeess']};
+            </div>    
+        </div>
+    
+    `;
+}
+
+function backToBoard() {
+    document.getElementById('overlayBg').classList.add('d-none');
 }
 
 // function deleteTask(i) {
