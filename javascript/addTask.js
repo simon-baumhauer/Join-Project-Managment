fcb02e72b441481beb7aa071b41f5107505346a7
JuergenHildbrand <jhildbrand@pm.me>
2022-02-25 11:13:13 +0100

    new

--- javascript/addTask.js
+++ javascript/addTask.js
@@ -19,3 +19,28 @@ function closeModal() {
     modal.classList.add('d-none')
     overlay.classList.add('d-none')
 }
+
+/**
+ * This function references to the form elements and assings the values to an Json object stores the Json object in an array. The array is stored in the browser.
+ * 
+ */
+ function createTask() {
+    // allTasks = [];
+    let title = document.getElementById('title').value;
+    let date = document.getElementById('date').value;
+    let catergory = document.getElementById('catergory').value;
+    let text = document.getElementById('text').value;
+    let urgency = document.getElementById('urgency').value;
+    let task = {
+        'title': title,
+        'date': date,
+        'catergory': catergory,
+        'text': text,
+        'urgency': urgency,
+        'createdAt': new Date().getTime()
+    }
+
+    allTasks.push(task);
+    allTaskAsString = JSON.stringify(allTasks);
+    localStorage.setItem('allTasks', allTaskAsString);
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,7 @@
 let allTasks = [];
 
 
-/**
- * This function references to the form elements and assings the values to an Json object stores the Json object in an array. The array is stored in the browser.
- * 
- */
-function createTask() {
-    // allTasks = [];
-    let title = document.getElementById('title').value;
-    let date = document.getElementById('date').value;
-    let catergory = document.getElementById('catergory').value;
-    let text = document.getElementById('text').value;
-    let urgency = document.getElementById('urgency').value;
-    let task = {
-        'title': title,
-        'date': date,
-        'catergory': catergory,
-        'text': text,
-        'urgency': urgency,
-        'createdAt': new Date().getTime()
-    }
 
-    allTasks.push(task);
-    allTaskAsString = JSON.stringify(allTasks);
-    localStorage.setItem('allTasks', allTaskAsString);
-}
 
 /**
  * This function accesses the stored data from the Browser and loads it again in the array
