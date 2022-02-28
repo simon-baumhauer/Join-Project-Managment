cbe3d85873ff548ce77189417dfe8e877dbc6bd5
navjotsingh96 <navjot.singh96@web.de>
2022-02-28 19:51:18 +0100

    corrected misktake

--- javascript/backlog.js
+++ javascript/backlog.js
@@ -1,5 +1,3 @@
-let backlogInfo = [];
-
 function pushToBacklog() {
 
     backlogInfo = JSON.parse(JSON.stringify(allTasks));
@@ -7,40 +5,48 @@ function pushToBacklog() {
 }
 
 function renderBacklogTasks() {
-
     let backlogContainer = document.getElementById('backlog_container');
     backlogContainer.innerHTML = '';
+    for (let index = 0; index < allTasks.length; index++) {
+        const info = allTasks[index];
 
+        let employe = info['assignEmployee'];
+        for (let z = 0; z < employe.length; z++) {
+            let emp = employe[z];
 
-    for (let index = 0; index < backlogInfo.length; index++) {
-        const info = backlogInfo[index];
-
-        backlogContainer.innerHTML += `
-        <div class="todo-container heading1">
-            <div class="person-info" id="person_info">
-            <div class="profile-img">
-                <img class="profile" id="profile_img" src=${info['assignEmployee'][0]['bild-src']}>
-             </div>
-            <div class="person-name">
-                 <h3 class="font-s-17 m-btm-2" id="person_name">${info['assignEmployee'][0]['name']}</h3>
-                 <span class="person-mail clr-blue font-s-14" id="person_mail">${info['assignEmployee'][0]['e-mail']}</span>
-             </div>
-             </div>
-             <div class="catagroy ctg-absolute">
-                  <h3 class="font-s-17" id="catagory">${info['catergory']}</h3>
-             </div>
-             <div class="details">
-             <div class="d-none chnge-container" id=chnge-container${index}>
-
-             <input type="text" id="text-edit${index}" class="d-none input-feld" value="${info['text']}">
-             
-             <button onclick="saveChanges(${index})">Save</button>
-             </div>
-             
-                  <span class="font-s-17" id="details${index}" onclick="edit_details(${index})">${info['text']}</span>
-            </div>
-            </div>`;
+            backlogContainer.innerHTML += renderTemplate(emp, info, index);
+        }
     }
+
+}
+
+function renderTemplate(emp, info, index) {
+    return `
+<div class="todo-container heading1">
+<div class="person-info" id="person_info">
+<div class="profile-img">
+    <img class="profile" id="profile_img" src=${emp['bild-src']}>
+ </div>
+<div class="person-name">
+     <h3 class="font-s-17 m-btm-2" id="person_name">${emp['name']}</h3>
+     <span class="person-mail clr-blue font-s-14" id="person_mail">${emp['e-mail']}</span>
+ </div>
+ </div>
+ <div class="catagroy ctg-absolute">
+      <h3 class="font-s-17" id="catagory">${info['catergory']}</h3>
+ </div>
+ <div class="details">
+ <div class="d-none chnge-container" id=chnge-container${index}>
+
+ <input type="text" id="text-edit${index}" class="d-none input-feld" value="${info['text']}">
+ 
+ <button onclick="saveChanges(${index})">Save</button>
+ </div>
+ 
+      <span class="font-s-17" id="details${index}" onclick="edit_details(${index})">${info['text']}</span>
+</div>
+</div>
+`;
 
 }
 
@@ -57,24 +63,27 @@ function edit_details(index) {
     edit.classList.remove('d-none');
     containeredit.classList.remove('d-none');
     hidedetails.classList.add('d-none');
-    console.log('hier ist allTasks', allTasks[index]['text']);
+    console.log(allTasks[index]['text']);
+
 
 }
 
-/* function saveChanges(index) {
+
+
+function saveChanges(index) {
 
     let text = document.getElementById('text-edit' + index).value;
     let detail = allTasks[index]['text'];
+
     let details = detail.push(text);
 
 
-    detail.splice(index, 1);
 
     let detailsAsString = JSON.stringify(details);
     localStorage.setItem('allTasks', detailsAsString)
-    console.log(backlogInfo[index]['text']);
+    console.log(allTasks[index]['text']);
     clickedSaved(index);
-} */
+}
 
 function clickedSaved(index) {
     let containeredit = document.getElementById('chnge-container' + index);
