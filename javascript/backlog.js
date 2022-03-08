70d244d991d6b2cefb561b480b7a83e6ea8731e6
navjotsingh96 <navjot.singh96@web.de>
2022-03-08 15:24:50 +0100

    corrcted employ

--- javascript/backlog.js
+++ javascript/backlog.js
@@ -37,31 +37,44 @@ async function loadBacklog() {
 
 function renderBacklogTasks() {
     let backlogContainer = document.getElementById('backlog_container');
-    backlogContainer.innerHTML = '';
+
+    backlogContainer.addEventListener("onclick", function tofo() {
+        console.log('Clicked');
+    });
     if (allTasks == '') {
         backlogContainer.innerHTML = noTasks();
     } else {
+
         for (let index = 0; index < allTasks.length; index++) {
             let info = allTasks[index];
+
             backlogContainer.innerHTML += renderTemplate(info, index);
 
             let employee = allTasks[index]['assignEmployee'];
             for (let j = 0; j < employee.length; j++) {
                 emp = employee[j];
-                const img = document.createElement("img");
+                const img = document.createElement("div");
                 const staff_name = document.createElement("h3");
                 const name_as_text = document.createTextNode(emp['name']);
                 staff_name.appendChild(name_as_text);
-                img.src = emp['bild-src'];
+                img.innerHTML += renderImage(emp);
 
-                document.getElementById(`person-name${index}`).appendChild(staff_name);
 
-                document.getElementById(`profile_img${index}`).appendChild(img);
+                let render = document.getElementById(`person-name${index}`);
+                render.innerHTML += renderImage(emp);
+                render.addEventListener("onclick", function bla() {
+                    console.log('blabla');
+
+                });
+
+
+                /* document.getElementById(`profile_img${index}`).appendChild(img); */
 
             }
         }
     }
 }
+
 
 
 
@@ -72,20 +85,27 @@ function noTasks() {
     </div>`;
 }
 
-{
-    /* <h3 class="font-s-17 m-btm-2" id="person_name">{emp['name']}</h3>
-    <span class="person-mail clr-blue font-s-14" id="person_mail">{emp['e-mail']}</span> */
+
+
+function renderImage(emp) {
+    return `
+    <div class="profile-img" id="${emp['name']}" onmouseover="mouseon(${emp['name']})">
+    <img src="${emp['bild-src']}">
+         <div class="emp-name">
+         <p> ${emp['name']}</p>
+        </div>
+ </div>
+ `;
 }
 
-
-function renderTemplate(info, index, emp) {
+function renderTemplate(info, index) {
     return `
 <div class="todo-container heading1">
 <div class="person-info" id="person_info">
 <div id="responsive${index}" class="responsive">
 <b>ASSIGNED TO</b>
 </div>
-<div class="profile-img" id="profile_img${index}" onmouseover="mouseOver(${index})" onmouseout="mouseOut(${index})">
+<div class="profile-img" id="profile_img${index}" onmouseout="mouseOut(${index})">
  </div>
 <div class="person-name" id="person-name${index}">
  </div>
@@ -177,7 +197,7 @@ function clickedSaved(index) {
     hidedetails.classList.remove('d-none');
 }
 
-function mouseOver(index) {
+/* function mouseOver(index) {
     let mouseon = document.getElementById('assigned-box' + index);
     mouseon.classList.remove('d-none');
     let employes = allTasks[index]['assignEmployee'];
@@ -186,6 +206,10 @@ function mouseOver(index) {
 
     }
 
+} */
+
+function Hoverbox(id) {
+    console.log('hier is');
 }
 
 function mouseOut(index) {
