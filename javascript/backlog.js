9c3b1b2b649ab4b9554e11345de0836a020e17fa
JuergenHildbrand <jhildbrand@pm.me>
2022-03-13 01:21:52 +0100

    cleanCode

--- javascript/backlog.js
+++ javascript/backlog.js
@@ -42,9 +42,9 @@ function forAssignEmploye(index) {
         const staff_name = document.createElement("h3");
         const name_as_text = document.createTextNode(emp['name']);
         staff_name.appendChild(name_as_text);
-        let render = document.getElementById(`person-name${index}`);
+        let render = document.getElementById(`employeeContainer${index}`);
         render.innerHTML += `
-        <div class="profile-img popup" id="${emp['name']}" onclick="popupBacklog(${index}, ${j})" >
+        <div class="employeeImg popup" id="${emp['name']}" onclick="popupBacklog(${index}, ${j})" >
            <img class="hover" src="${emp['bild-src']}">
           <div class="popuptext" id="myPopup${index}, ${j}">
             ${emp['name']}<br>
@@ -52,7 +52,7 @@ function forAssignEmploye(index) {
               ${emp['e-mail']}
              </div>
         `;
-        heightofContainer(employee, index);
+        // heightofContainer(employee, index);
     }
 }
 
@@ -68,7 +68,7 @@ function popupBacklog(index, j) {
  */
 function noTasks() {
     return `
-    <div class="todo-container heading1" style="justify-content:center;">
+    <div class="todoContainer" style="justify-content:center;">
     Keine Tasks mehr
     </div>`;
 }
@@ -104,38 +104,79 @@ function noTasks() {
  */
 function renderTemplate(info, index) {
     return `
-                <div class="todo-container heading1" id="todo${index}">
-                <div class="person-info" id="person_info">
-                <div id="responsive${index}" class="responsive ass1">
-                <b>ASSIGNED TO</b>
+            <div class="todoContainer" id="todo${index}">
+                <div class="responsive assignedToTasks fontS20">
+                    <b>ASSIGNED TO</b>
                 </div>
-                <div class="profile-img" id="profile_img${index}">
+                <div class="employeeContainer" id="employeeContainer${index}"></div>
+                <div class="responsive fontS20">
+                    <b>CATEGORY</b>
                 </div>
-                <div class="person-name" id="person-name${index}">
+                <div class="catogeryTasks">
+                    <h3 >${info['catergory']}</h3>
                 </div>
+                <div class="responsive fontS20">
+                    <b>DETAILS</b>
                 </div>
-                <div class="assigend-to d-none" id="assigned-box${index}">
+                <div class="detailsTasks flipped" onclick="changeContainer(${info})">
+                    ${info['text']}  
                 </div>
-                <div class="catagroy ctg-absolute">
-                <div id="responsive${index}" class="responsive">
-                <b>CATEGORY</b>
+                <div class="responsive fontS20">
+                    <b>Due Date</b>
                 </div>
-                    <h3 class="cat2 font-s-17" id="catagroy${index}">${info['catergory']}</h3>
+                <div class="dueDateTasks">
+                    ${info['date']}
                 </div>
-                <div class="details">
-                <div id="responsive${index}" class="responsive">
-                <b>DETAILS</b>
-                </div>
-                <div class="d-none chnge-container" id=chnge-container${index}>
-                <input type="text" id="text-edit${index}" class="d-none input-feld" value="${info['text']}">
-                <button onclick="saveChanges(${index})">Save</button>
-                </div>               
-                    <span class="det2 font-s-17" id="details${index}" onclick="edit_details(${index})">${info['text']}</span>
-                </div>
-                <div class="dat2">${info['date']}</div>
-                <img class="pushToBoard" src="img/arrowToBoard.ico" onclick="pushToBoard(${index})">
-            `;
+                <img class="pushToBoard" src="img/arrowToBoard.ico" onclick="pushToBoard(${index})"> 
+            </div>
+                
+        `;
 }
+
+function changeContainer(i) {
+    document.getElementById(`changeContainer${i}`).classList.remove('d-none');
+}
+
+
+
+/* <div class="d-none changeContainer" id="changeContainer">
+                        <textarea type="text" id="text-edit${index}" class="d-none input-feld" value="${info['text']}">
+                        <button onclick="saveChanges(${index})">Save</button>
+                    // </div>   */
+
+                // <div class="todo-container heading1" id="todo${index}">
+                // <div class="person-info" id="person_info">
+
+                // <div id="responsive${index}" class="responsive ass1">
+                // <b>ASSIGNED TO</b>
+                // </div>
+
+                // <div class="profile-img" id="profile_img${index}">
+                // </div>
+                // <div class="person-name" id="person-name${index}">
+                // </div>
+                // </div>
+                // <div class="assigend-to d-none" id="assigned-box${index}">
+                // </div>
+                // <div class="catagroy ctg-absolute">
+                // <div id="responsive${index}" class="responsive">
+                // <b>CATEGORY</b>
+                // </div>
+                //     <h3 class="cat2 font-s-17" id="catagroy${index}">${info['catergory']}</h3>
+                // </div>
+                // <div class="details">
+                // <div id="responsive${index}" class="responsive">
+                // <b>DETAILS</b>
+                // </div>
+                // <div class="d-none chnge-container" id=chnge-container${index}>
+                // <input type="text" id="text-edit${index}" class="d-none input-feld" value="${info['text']}">
+                // <button onclick="saveChanges(${index})">Save</button>
+                // </div>               
+                //     <span class="det2 font-s-17" id="details${index}" onclick="edit_details(${index})">${info['text']}</span>
+                // </div>
+                // <div class="dat2">${info['date']}</div>
+                // <img class="pushToBoard" src="img/arrowToBoard.ico" onclick="pushToBoard(${index})"></img>
+
 /**
  * 
  * @param {Paramter from borrd.js} i 
@@ -148,43 +189,43 @@ async function pushToBoard(i) {
     await backend.setItem('allTasks', JSON.stringify(allTasks));
     renderBacklogTasks();
 }
-/**
- * 
- * @param {this is employee Array only named pax} pax 
- * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
- * This function decided height of container if there are more then 3 Assigned Employe in an array or less than 3. 
- */
-function heightofContainer(pax, index) {
-    if (pax.length <= 3) {
-        document.getElementById(`todo${index}`).style.height = "70px";
-        document.getElementById(`catagroy${index}`).style.top = "40px";
-        console.log('catagroy${index} ')
-    }
-    if (pax.length >= 4) {
-        document.getElementById(`todo${index}`).style.height = "140px";
+// /**
+//  * 
+//  * @param {this is employee Array only named pax} pax 
+//  * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
+//  * This function decided height of container if there are more then 3 Assigned Employe in an array or less than 3. 
+//  */
+// function heightofContainer(pax, index) {
+//     if (pax.length <= 3) {
+//         document.getElementById(`todo${index}`).style.height = "70px";
+//         document.getElementById(`catagroy${index}`).style.top = "40px";
+//         console.log('catagroy${index} ')
+//     }
+//     if (pax.length >= 4) {
+//         document.getElementById(`todo${index}`).style.height = "140px";
 
-    }
-}
-/**
- * 
- * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
- * it helps to scroll right if user clicks on right Arrow
- */
-function scrollright(index) {
-    document.getElementById(`left-scrollbar${index}`).classList.remove('d-none');
-    document.getElementById(`left-scrollbar${index}`);
-    let content = document.getElementById(`person-name${index}`);
-    content.style.scrollBehavior = 'smooth';
-    content.scrollLeft += 200;
-}
-/**
- * 
- * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
- * it helps to scroll left if user clicks on left Arrow
- * 
- */
-function scrollleft(index) {
-    let content = document.getElementById(`person-name${index}`);
-    content.style.scrollBehavior = 'smooth';
-    content.scrollLeft -= 200;
-}
+//     }
+// }
+// /**
+//  * 
+//  * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
+//  * it helps to scroll right if user clicks on right Arrow
+//  */
+// function scrollright(index) {
+//     document.getElementById(`left-scrollbar${index}`).classList.remove('d-none');
+//     document.getElementById(`left-scrollbar${index}`);
+//     let content = document.getElementById(`person-name${index}`);
+//     content.style.scrollBehavior = 'smooth';
+//     content.scrollLeft += 200;
+// }
+// /**
+//  * 
+//  * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
+//  * it helps to scroll left if user clicks on left Arrow
+//  * 
+//  */
+// function scrollleft(index) {
+//     let content = document.getElementById(`person-name${index}`);
+//     content.style.scrollBehavior = 'smooth';
+//     content.scrollLeft -= 200;
+// }
