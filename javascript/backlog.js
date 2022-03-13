695174f88f5ddb5ce678f9038b80962c95477da9
JuergenHildbrand <jhildbrand@pm.me>
2022-03-13 03:09:16 +0100

    cleanCode

--- javascript/backlog.js
+++ javascript/backlog.js
@@ -1,8 +1,6 @@
 setURL('http://gruppe-177.developerakademie.net/smallest_backend_ever');
-/**
- * A array to load from Server
- */
 let boardArray = [];
+
 /**
  *  to load and show saved taskes from Server
  */
@@ -12,6 +10,7 @@ async function loadBacklog() {
     boardArray = JSON.parse(backend.getItem('boardArray')) || [];
     renderBacklogTasks();
 }
+
 /**
  * This function render the Code to HTML
  */
@@ -28,39 +27,6 @@ function renderBacklogTasks() {
         }
     }
 }
-/**
- * 
- * @param {This paramter gives each employee its own number so that the funtion scope is only for the the seletected employee} index 
-    this is a for loop for assigningEmployees and this function renders the images and details from assignEmployee array
- * 
- */
-function forAssignEmploye(index) {
-    let employee = allTasks[index]['assignEmployee'];
-    for (let j = 0; j < employee.length; j++) {
-        emp = employee[j];
-        const img = document.createElement("div");
-        const staff_name = document.createElement("h3");
-        const name_as_text = document.createTextNode(emp['name']);
-        staff_name.appendChild(name_as_text);
-        let render = document.getElementById(`employeeContainer${index}`);
-        render.innerHTML += `
-        <div class="employeeImg popup" id="${emp['name']}" onclick="popupBacklog(${index}, ${j})" >
-           <img class="hover" src="${emp['bild-src']}">
-          <div class="popuptext" id="myPopup${index}, ${j}">
-            ${emp['name']}<br>
-             ${emp['position']}<br>
-              ${emp['e-mail']}
-             </div>
-        `;
-        // heightofContainer(employee, index);
-    }
-}
-
-function popupBacklog(index, j) {
-    let popup = document.getElementById(`myPopup${index}, ${j}`);
-    popup.classList.toggle("show");
-}
-
 
 /**
  * 
@@ -72,29 +38,6 @@ function noTasks() {
     Keine Tasks mehr
     </div>`;
 }
-// When the user clicks on Id popup(number), it opens the popup
-/**
- * 
- *This function helps to show the images and names of the Employe with the help forscheleife 
- */
-// function renderDetails(emp, index, j) {
-//     return `
-//     <div class="profile-img popup" id="${emp['name']}" onclick="popupBacklog(${index}${j})" >
-//     <img src="${emp['bild-src']}">
-//     <div class="popuptext" id="myPopup${index}${j}">
-//      ${emp['name']}<br>
-//      ${emp['position']}<br>
-//      ${emp['e-mail']}
-//      </div>
-//      <div class="emp-name">
-//      <p> ${emp['name']}</p>
-//      </div> 
-//  </div>
-//  `;
-// }
-// popup.classList.toggle("show");
-
-
 
 /**
  * 
@@ -137,10 +80,46 @@ function renderTemplate(info, index) {
         `;
 }
 
+/**
+ * 
+ * @param {This paramter gives each employee its own number so that the funtion scope is only for the the seletected employee} index 
+    this is a for loop for assigningEmployees and this function renders the images and details from assignEmployee array
+ * 
+ */
+function forAssignEmploye(index) {
+    let employee = allTasks[index]['assignEmployee'];
+    for (let j = 0; j < employee.length; j++) {
+        emp = employee[j];
+        const img = document.createElement("div");
+        const staff_name = document.createElement("h3");
+        const name_as_text = document.createTextNode(emp['name']);
+        staff_name.appendChild(name_as_text);
+        let render = document.getElementById(`employeeContainer${index}`);
+        render.innerHTML += `
+            <div class="employeeImg popup" id="${emp['name']}" onclick="popupBacklog(${index}, ${j})">
+                <img class="hover" src="${emp['bild-src']}">
+                <div class="popuptext" id="myPopup${index}, ${j}">
+                    ${emp['name']}<br>
+                    ${emp['position']}<br>
+                    ${emp['e-mail']}
+                </div>
+            </div>    
+        `;
+    }
+}
+
+
+// When the user clicks on Id popup(number), it opens the popup
+function popupBacklog(index, j) {
+    let popup = document.getElementById(`myPopup${index}, ${j}`);
+    popup.classList.toggle("show");
+}
+
+
 function changeContainer(i) {
     document.getElementById('textEditCont' + i).classList.remove('d-none');
     let currentText = allTasks[i]['text'];
-    document.getElementById('textEdit'+ i).innerHTML = currentText;
+    document.getElementById('textEdit' + i).innerHTML = currentText;
 }
 
 async function saveChanges(i) {
@@ -150,46 +129,6 @@ async function saveChanges(i) {
     document.getElementById('textEditCont' + i).classList.add('d-none');
     loadBacklog();
 }
-
-
-
-/* <div class="d-none changeContainer" id="changeContainer">
-                        <textarea type="text" id="text-edit${index}" class="d-none input-feld" value="${info['text']}">
-                        <button onclick="saveChanges(${index})">Save</button>
-                    // </div>   */
-
-                // <div class="todo-container heading1" id="todo${index}">
-                // <div class="person-info" id="person_info">
-
-                // <div id="responsive${index}" class="responsive ass1">
-                // <b>ASSIGNED TO</b>
-                // </div>
-
-                // <div class="profile-img" id="profile_img${index}">
-                // </div>
-                // <div class="person-name" id="person-name${index}">
-                // </div>
-                // </div>
-                // <div class="assigend-to d-none" id="assigned-box${index}">
-                // </div>
-                // <div class="catagroy ctg-absolute">
-                // <div id="responsive${index}" class="responsive">
-                // <b>CATEGORY</b>
-                // </div>
-                //     <h3 class="cat2 font-s-17" id="catagroy${index}">${info['catergory']}</h3>
-                // </div>
-                // <div class="details">
-                // <div id="responsive${index}" class="responsive">
-                // <b>DETAILS</b>
-                // </div>
-                // <div class="d-none chnge-container" id=chnge-container${index}>
-                // <input type="text" id="text-edit${index}" class="d-none input-feld" value="${info['text']}">
-                // <button onclick="saveChanges(${index})">Save</button>
-                // </div>               
-                //     <span class="det2 font-s-17" id="details${index}" onclick="edit_details(${index})">${info['text']}</span>
-                // </div>
-                // <div class="dat2">${info['date']}</div>
-                // <img class="pushToBoard" src="img/arrowToBoard.ico" onclick="pushToBoard(${index})"></img>
 
 /**
  * 
@@ -203,43 +142,3 @@ async function pushToBoard(i) {
     await backend.setItem('allTasks', JSON.stringify(allTasks));
     renderBacklogTasks();
 }
-// /**
-//  * 
-//  * @param {this is employee Array only named pax} pax 
-//  * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
-//  * This function decided height of container if there are more then 3 Assigned Employe in an array or less than 3. 
-//  */
-// function heightofContainer(pax, index) {
-//     if (pax.length <= 3) {
-//         document.getElementById(`todo${index}`).style.height = "70px";
-//         document.getElementById(`catagroy${index}`).style.top = "40px";
-//         console.log('catagroy${index} ')
-//     }
-//     if (pax.length >= 4) {
-//         document.getElementById(`todo${index}`).style.height = "140px";
-
-//     }
-// }
-// /**
-//  * 
-//  * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
-//  * it helps to scroll right if user clicks on right Arrow
-//  */
-// function scrollright(index) {
-//     document.getElementById(`left-scrollbar${index}`).classList.remove('d-none');
-//     document.getElementById(`left-scrollbar${index}`);
-//     let content = document.getElementById(`person-name${index}`);
-//     content.style.scrollBehavior = 'smooth';
-//     content.scrollLeft += 200;
-// }
-// /**
-//  * 
-//  * @param {This paramter gives each employee its own number so that the funtion scope is only for the seleted employee} index 
-//  * it helps to scroll left if user clicks on left Arrow
-//  * 
-//  */
-// function scrollleft(index) {
-//     let content = document.getElementById(`person-name${index}`);
-//     content.style.scrollBehavior = 'smooth';
-//     content.scrollLeft -= 200;
-// }
